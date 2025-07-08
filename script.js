cc83637c7ca8bbff1b2e38297da18f8e6f133350
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-07-08 10:09:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function minDate(dates) {
   //write you code here
+	let minDate =dates[0];
+
+	for(let i=0;i<dates.length;i++){
+		if(dates[i]<minDate){
+			minDate=dates[i];
+		}
+	}
+	return minDate;
+	
 }
 
 // Do not change the code
